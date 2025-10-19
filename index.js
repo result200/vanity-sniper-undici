7a1befa833894af45b9b28439f84308bc341b0d3
result200 <234462180+result200@users.noreply.github.com>
2025-10-19 19:10:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-yapayim mi=?
-5 star pub
+const getHeaders = () => ({
+  'Authorization': token,
+  'Content-Type': 'application/json',
+  'X-Discord-MFA-Authorization': mfaToken,
+  'X-Audit-Log-Reason': '',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0',
+  'X-Super-Properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRmlyZWZveCIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJ0ci1UUiIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQ7IHJ2OjEzMy4wKSBHZWNrby8yMDEwMDEwMSBGaXJlZm94LzEzMy4wIiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTMzLjAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6Imh0dHBzOi8vd3d3Lmdvb2dsZS5jb20vIiwicmVmZXJyaW5nX2RvbWFpbiI6Ind3dy5nb29nbGUuY29tIiwic2VhcmNoX2VuZ2luZSI6Imdvb2dsZSIsInJlZmVycmVyX2N1cnJlbnQiOiIiLCJyZWZlcnJpbmdfZG9tYWluX2N1cnJlbnQiOiIiLCJyZWxlYXNlX2NoYW5uZWwiOiJjYW5hcnkiLCJjbGllbnRfYnVpbGRfbnVtYmVyIjozNTYxNDAsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGwsImhhc19jbGllbnRfbW9kcyI6ZmFsc2V9'
+});
+
+const url = `https://canary.discord.com/api/v9/guilds/${serverId}/vanity-url`;
+
+const sendVanityRequest = async (vanityCode) => {
+  try {
+    const response = await fetch(url, {
+      method: 'PATCH',
+      headers: getHeaders(),
+      body: JSON.stringify({ code: vanityCode }),
+      dispatcher: agent
+    });
+    
+    const data = await response.json();
+    if (data.code || data.message) {
+      console.log('✅ undici:', JSON.stringify(data), '| Vanity:', vanityCode);
+    }
+  } catch (e) {
+    console.log('❌ Error:', e.message);
+  }
+}
